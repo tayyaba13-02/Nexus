4c826153fd98c43814a81f56e74a340cc51cd0b3
Tayyaba Anwar <tayyabaanwar0820@gmail.com>
2026-01-21 01:58:30 +0500

    Final push: Syncing all recent fixes and frontend assets to Hugging Face

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pc2odl15fu8"
+    "revision": "0.lcj13ijkdgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
